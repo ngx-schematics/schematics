0e45c593d35bc76c5fabe7ffc26e6d97eb47eb1c
Michael Wooten <mwooten.dev@gmail.com>
2020-09-05 21:15:59 +0000

    Added a library project to the workspace.

---
+++ packages/sandbox/projects/sandbox-lib/src/lib/sandbox-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sb-sandbox-lib',
+  template: `
+    <p>
+      sandbox-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SandboxLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ packages/sandbox/projects/sandbox-lib/src/lib/sandbox-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SandboxLibComponent } from './sandbox-lib.component';
+
+
+
+@NgModule({
+  declarations: [SandboxLibComponent],
+  imports: [
+  ],
+  exports: [SandboxLibComponent]
+})
+export class SandboxLibModule { }
